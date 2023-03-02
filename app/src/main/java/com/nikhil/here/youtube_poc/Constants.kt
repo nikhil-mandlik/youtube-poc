c3a7adda2c0c71dddaf6aba2099d6606915703f5
nikhil-mandlik <nikhilmandlik@sharechat.co>
2023-03-02 19:59:44 +0530

    hiding api keys

--- app/src/main/java/com/nikhil/here/youtube_poc/Constants.kt
+++ app/src/main/java/com/nikhil/here/youtube_poc/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     const val MojProfileUri = "https://img.freepik.com/premium-vector/moj-logo_578229-275.jpg?w=826"
     const val MojDisplayName = "Moj User"
     const val MojEmail = "unknown"
-    const val ApiKey = "AIzaSyD29qEryxgXtD_ZLnxXSTvfRc1IPZTUKUM"
+    const val ApiKey = "<YOUR_API_KEY>"
 }
